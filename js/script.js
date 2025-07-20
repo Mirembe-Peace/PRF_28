0a0322e0942df5e3b7d47e5645df6e8c10548a03
Mirembe Peace Mercy <peace2de@gmail.com>
2025-07-20 15:21:44 +0300

    lights

--- js/script.js
+++ js/script.js
@@ -28,13 +28,14 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(-165.01445413093128, 539.25437520156, -216.11550290035518);
 ambientLight.position.set(86.73729926481377, 140.41787049838712, 17.54735020570745);
-scene.add(ambientLight);
-scene.add(directionalLight);
 
 if(isMobile) {
     ambientLight.intensity = 5;
     directionalLight.intensity = 5;
 }
+
+scene.add(ambientLight);
+scene.add(directionalLight);
 
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 0.5;
