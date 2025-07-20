5517505824681e4517791dff7c9db831abfea982
Mirembe Peace Mercy <peace2de@gmail.com>
2025-07-20 23:22:56 +0300

    mobile iframe css

--- js/script.js
+++ js/script.js
@@ -199,7 +199,7 @@ function addControls() {
         hitTestDistance: 40    // distance to test for hit
     }
     controls = new TouchControls(container.parentNode, camera, options);
-    controls.setPosition(84, 45, 54);
+    controls.setPosition(84, 34, 40);
     controls.addToScene(scene);
     
 }
