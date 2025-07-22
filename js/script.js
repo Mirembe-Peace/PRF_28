ca247a832aba0820bdad060cac6753cc688577b9
Mirembe Peace Mercy <peace2de@gmail.com>
2025-07-22 15:15:29 +0300

    changed youtube iframe for desktop

--- js/script.js
+++ js/script.js
@@ -510,7 +510,7 @@ function showYouTubeVideo_1(videoId) {
         videoContainer_1.style.height = '30%';
         } else {
             videoContainer_1.style.top = '70px';
-            videoContainer_1.style.left = '70px';
+            videoContainer_1.style.right = '70px';
             videoContainer_1.style.width = '70%';
             videoContainer_1.style.height = '50%';
 
