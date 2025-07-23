bdbd11bd0250c203bcd0a0a4ba9e1823c7314af8
Mirembe Peace Mercy <peace2de@gmail.com>
2025-07-23 18:36:06 +0300

    typo 'h'

--- js/RotationPad.js
+++ js/RotationPad.js
@@ -79,7 +79,6 @@ class RotationPad {
         this.padElement.style.top = canvas.height + this.container.getBoundingClientRect().top
                                     - this.region.offsetHeight - 10 + 'px'
         this.padElement.style.left = canvas.offsetWidth - this.region.offsetWidth - 20 + 'px'
-h
         this.regionData.width = this.region.offsetWidth
         this.regionData.height = this.region.offsetHeight
         this.regionData.position = {
