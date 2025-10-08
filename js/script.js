604fe26c4ad4de9bdc9c9b09194e24e8a2857311
Mirembe Peace Mercy <peace2de@gmail.com>
2025-10-08 12:01:58 +0300

    changed the model to UCU library

--- js/script.js
+++ js/script.js
@@ -375,7 +375,7 @@ function initControls() {
     const gltfLoader = new GLTFLoader(loadingManager);
 
     gltfLoader.load(
-        'https://storage.googleapis.com/pearl-artifacts-cdn/scene.gltf',
+        'https://storage.googleapis.com/ucu_library/librarygltf/library.gltf',
         function (gltf) {
             const museum = gltf.scene;
             museum.position.set(0, 0, 0);
